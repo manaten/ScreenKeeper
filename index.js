1887e9d99c770cf0dcfe00d67dae92f9b7f9123b
manaten <manaten@manaten.net>
2020-10-11 22:16:50 +0900

    feat(robot): initial commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+const robot = require("robotjs");
+
+const WAIT_MSEC = 1000 * 60 * 10;
+
+const wait = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+const main = async () => {
+  while(true) {
+    try {
+      const screenSize = robot.getScreenSize();
+      const mouse = robot.getMousePos();
+
+      const newX = mouse.x + 1 < screenSize.width ? mouse.x + 1 : mouse.x - 1;
+      const newY = mouse.y;
+
+      robot.moveMouse(newX, newY);
+      console.log(`move to (${newX}, ${newY}).`);
+    } catch(e) {
+      console.error(e);
+    }
+    await wait(WAIT_MSEC);
+  }
+}
+main();
