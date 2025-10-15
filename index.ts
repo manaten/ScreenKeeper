6122ae34dfb146e3b74c9c411751e5a586591228
manaten <586064+manaten@users.noreply.github.com>
2025-10-15 10:26:09 +0900

    chore: typescript (#2)
    * chore: typescript
    
    * chore: fix ci.yml for robotjs

---
+++ index.ts
@@ -0,0 +1,37 @@
+import robot from "robotjs";
+
+const WAIT_MSEC = 1000 * 20;
+
+const wait = (msec: number) =>
+  new Promise((resolve) => setTimeout(resolve, msec));
+const log = (message: string) =>
+  console.log(`[${new Date().toLocaleString()}] ${message}`);
+
+let oldX = 0; // eslint-disable-line functional/no-let
+
+const main = async () => {
+  while (true) {
+    try {
+      const screenSize = robot.getScreenSize();
+      const mouse = robot.getMousePos();
+
+      if (oldX === mouse.x) {
+        const newX =
+          mouse.x + 1 >= screenSize.width || mouse.x % 2 === 0
+            ? mouse.x - 1
+            : mouse.x + 1;
+        const newY = mouse.y;
+
+        robot.moveMouse(newX, newY);
+        log(`move to (${newX}, ${newY}).`);
+        oldX = newX;
+      } else {
+        oldX = mouse.x;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    await wait(WAIT_MSEC);
+  }
+};
+main();
